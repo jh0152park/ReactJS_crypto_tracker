7699b5ad2d2c8199344430679c193f747d7529fb
jh0152park <jh0152park@naver.com>
2023-07-02 22:58:59 +0900

    added overview

--- crypto_tracker/src/screen/Coin.tsx
+++ crypto_tracker/src/screen/Coin.tsx
@@ -28,14 +28,93 @@ const Loader = styled.div`
   font-weight: bold;
 `;
 
+const Overview = styled.div`
+  width: 100%;
+  height: 50px;
+  border-radius: 15px;
+  background-color: #636e72;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 20px;
+  text-align: center;
+  font-weight: bold;
+  p:nth-child(1) {
+    font-size: 10px;
+    margin-bottom: 5px;
+  }
+`;
+
+const Description = styled.p`
+  color: whitesmoke;
+  font-size: 15px;
+  margin: 20px 0px;
+`;
+
 interface RouteState {
   name: string;
 }
 
+interface InfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface PriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      ath_date: string;
+      ath_price: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_1h: number;
+      percent_change_1y: number;
+      percent_change_6h: number;
+      percent_change_7d: number;
+      percent_change_12h: number;
+      percent_change_15m: number;
+      percent_change_24h: number;
+      percent_change_30d: number;
+      percent_change_30m: number;
+      percent_from_price_ath: number;
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+    };
+  };
+}
+
 function Coin() {
   const [loading, setLoading] = useState(true);
-  const [info, setInfo] = useState({});
-  const [price, setPrice] = useState({});
+  const [info, setInfo] = useState<InfoData>();
+  const [price, setPrice] = useState<PriceData>();
 
   const { coinId } = useParams<{ coinId: string }>();
   const { state } = useLocation<RouteState>();
@@ -51,6 +130,10 @@ function Coin() {
       ).json();
       setInfo(infoData);
       setPrice(priceData);
+      setLoading(false);
+
+      console.log(infoData);
+      console.log(priceData);
     })();
   }, []);
 
@@ -60,7 +143,37 @@ function Coin() {
         <Title>{state?.name || "Loading"}</Title>
       </Header>
 
-      {/* {loading ? <Loader>Loading...</Loader> : null} */}
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Overview>
+            <div>
+              <p>Rank</p>
+              <p>{info?.rank}</p>
+            </div>
+            <div>
+              <p>Symbol</p>
+              <p>{info?.symbol}</p>
+            </div>
+            <div>
+              <p>Type</p>
+              <p>{info?.type}</p>
+            </div>
+          </Overview>
+          <Description>{info?.description}</Description>
+          <Overview>
+            <div>
+              <p>Total Suply</p>
+              <p>{price?.total_supply}</p>
+            </div>
+            <div>
+              <p>Max Suply</p>
+              <p>{price?.max_supply}</p>
+            </div>
+          </Overview>
+        </>
+      )}
     </Container>
   );
 }
